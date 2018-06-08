60cee5aca36b20716290ae1bd2d789ce52435a72
jadope <jeronimoal10@gmail.com>
2018-06-08 18:54:46 -0500

    Last upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')  
 const uuid = require("node-uuid");
-const port = process.env.SPECIAL_PORT || 1234 
+const port = process.env.SPECIAL_PORT || 4321 
 
 const requestHandler = (request, response) => {  
   const message = `¡Hola! Estamos generando un uuid con base en un número aleatorio y a ti te tocó el: ${uuid.v4()}, si no te gusta podrías recargar la página para obtener otro.`;
